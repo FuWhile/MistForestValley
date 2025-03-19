907eae6b64a3250cd78e1bba53fa645bb543d3c4
FuWhile <horacewong410@gmail.com>
2025-03-20 01:15:26 +0900

    修复 router.js，添加 Home.vue

--- mistforestvalley-web/src/main.js
+++ mistforestvalley-web/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // 🚀 确保 router 被引入
+console.log("✅ 当前注册的路由:", router.getRoutes());
+
 
 const app = createApp(App);
 app.use(router);  // 🚀 绑定 Vue Router
