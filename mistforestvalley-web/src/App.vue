34265ef071f5a3a569df2422beb42e8ad9837a12
FuWhile <horacewong410@gmail.com>
2025-03-20 00:38:16 +0900

    change app.vue

--- mistforestvalley-web/src/App.vue
+++ mistforestvalley-web/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <h1>欢迎回到雾森谷：Mist Forest Valley</h1>
-    <Login />
+    <!-- 
+      重要：使用 <router-view /> 来渲染当前路由匹配到的组件。
+      当你访问 / 时，会显示 Login.vue
+      当你访问 /home 时，会显示 Home.vue
+      ...
+    -->
+    <router-view />
   </div>
 </template>
 
 <script setup>
-import Login from "./components/Login.vue";
+// 这里可以暂时不需要任何脚本逻辑
 </script>
 
 <style>
@@ -15,3 +21,4 @@ h1 {
   margin-top: 50px;
 }
 </style>
+
